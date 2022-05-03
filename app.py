ef1e3560884f73775a9d02a3d5de1122db73297e
MikesoWeb <klionpsyx@gmail.com>
2022-05-03 17:52:41 +0300

    'update_project'

--- app.py
+++ app.py
@@ -1,53 +1,6 @@
-from flask import Flask, render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-
-# /// = relative path, //// = absolute path
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
-
-
-class ToDo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100))
-    is_complete = db.Column(db.Boolean)
-
-
-db.create_all()
-
-
-@app.get('/')
-def home():
-    todo_list = ToDo.query.all()
-    return render_template('index.html', todo_list=todo_list, title='Главная страница')
-
-
-@app.post('/add')
-def add():
-    title = request.form.get('title')
-    new_todo = ToDo(title=title, is_complete=False)
-    db.session.add(new_todo)
-    db.session.commit()
-    return redirect(url_for('home'))
-
-
-@app.get('/update/<int:todo_id>')
-def update(todo_id):
-    todo = ToDo.query.filter_by(id=todo_id).first()
-    todo.is_complete = not todo.is_complete
-    db.session.commit()
-    return redirect(url_for('home'))
-
-
-@app.get('/delete/<int:todo_id>')
-def delete(todo_id):
-    todo = ToDo.query.filter_by(id=todo_id).first()
-    db.session.delete(todo)
-    db.session.commit()
-    return redirect(url_for('home'))
-
+from todo.routes import app, db
 
 if __name__ == '__main__':
-    app.run()
+    with app.app_context():
+        db.create_all()
+        app.run(debug=True, port=5555)
